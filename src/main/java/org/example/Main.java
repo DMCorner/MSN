b2f4378c1862714c76db988a947cee3bc4055d57
DANIEL CORNER <danielmichaelcorner@gmail.com>
2022-11-21 10:42:33 +0000

    not vim please

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+// firstClick still not created // unnecessary
+// accepting strings bug? as 1 1
+// boundary issues when hitting bomb on edge && height != width // nic fixed in like 10 seconds
+//
 public class Main {
+    // keep this place clean
+    // stop putting all code here
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Minesweeper game = new Minesweeper();
+        game.startMineSweeper();
     }
 }
